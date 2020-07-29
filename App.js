49c8e5f1cefac32b183b9c447b8aca70b553ef64
Adam Toczko <adam.toczko@signia.group>
2020-07-29 13:33:16 +0200

    basic tries

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Image
+                style={styles.image}
+                source={{ uri: "https://picsum.photos/id/237/200/300" }}
+            />
+            <Text style={styles.text}>
+                To share a photo from your phone {"\n"} with a friend, just
+                press the button below!
+            </Text>
+            <TouchableOpacity
+                onPress={() => alert("Pick again !")}
+                style={{ marginTop: 20, backgroundColor: "#56CCF2" }}
+            >
+                <Text style={{ fontSize: 20, color: "#fff" }}>
+                    Pick a photo
+                </Text>
+            </TouchableOpacity>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: " rgb(250,250,250)",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        paddingBottom: 10,
+    },
+    image: {
+        marginTop: 40,
+        marginBottom: 20,
+        width: 200,
+        height: 200,
+    },
+    text: {
+        textAlign: "center",
+        fontSize: 20,
+    },
 });
